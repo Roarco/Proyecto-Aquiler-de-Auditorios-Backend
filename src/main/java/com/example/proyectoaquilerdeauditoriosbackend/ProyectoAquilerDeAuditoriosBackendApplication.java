2739b99cc60a18bfe3ee10a9d73dd7bc52247947
Roarco <sebuscaroberth@gmail.com>
2022-09-30 21:14:24 -0500

    Inicializando el proyecto con spring boot , jpa y configurando el archivo (aplication.properties) para la conexion con la base de datos

---
+++ src/main/java/com/example/proyectoaquilerdeauditoriosbackend/ProyectoAquilerDeAuditoriosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoaquilerdeauditoriosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAquilerDeAuditoriosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAquilerDeAuditoriosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoaquilerdeauditoriosbackend/ProyectoAquilerDeAuditoriosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoaquilerdeauditoriosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAquilerDeAuditoriosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
